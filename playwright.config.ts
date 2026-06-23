7c1596e11216cadb446db5c535d61a341ab19bf6
naveenautomationlabs <naveenautomationlabs@MacBook-Pro-3.local>
2026-06-23 08:04:11 +0530

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? '50%' : undefined,
 
   reporter: [
     ["list"],
@@ -32,7 +32,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
-    headless: false,
+    headless: !process.env.CI ? false : true,
   },
 
   /* Configure projects for major browsers */
